6e74bb8c87f49469f98558ebe0411c0daea11dc1
Allan Resende <allanresendegomes@outlook.com>
2026-06-18 22:52:51 -0300

    Backend - Add FormRequests and Services for Adress and Patient

---
+++ backend/app/Services/AddressService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+class AddressService
+{
+    public function store(array $data): void
+    {
+        //
+    }
+
+    public function update(int $id, array $data): void
+    {
+        //
+    }
+
+    public function delete(int $id): void
+    {
+        //
+    }
+}
---
+++ backend/app/Services/PatientService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+class PatientService
+{
+    public function store(array $data): void
+    {
+        //
+    }
+
+    public function update(int $id, array $data): void
+    {
+        //
+    }
+
+    public function delete(int $id): void
+    {
+        //
+    }
+}
