5c73550e1003d0472d038259f84cb3606c4991d6
Allan Resende <allanresendegomes@outlook.com>
2026-06-19 01:45:34 -0300

    Backend -  Update API routes: Add adresses and patients routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\AddressController;
+use App\Http\Controllers\Api\PatientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::get("/", [ApiController::class, 'index']);
+Route::apiResource('addresses', AddressController::class);
+Route::apiResource('patients', PatientController::class);
