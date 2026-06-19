24a8b86861f32fa36b3f09185be5389ae3158133
Allan Resende <allanresendegomes@outlook.com>
2026-06-19 00:14:22 -0300

    Backend - Implement Address and Patient form request validation for Store and Update

--- backend/app/Http/Requests/StorePatientRequest.php
+++ backend/app/Http/Requests/StorePatientRequest.php
@@ -6,25 +6,45 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StorePatientRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'name' => 'required|string|max:255',
+      'cpf' => [
+        'required',
+        'string',
+        'size:11',
+        'regex:/^[0-9]+$/',
+        'cpf',
+        'unique:patients,cpf'
+      ],
+      'cns' => [
+        'required',
+        'string',
+        'size:15',
+        'regex:/^[0-9]+$/',
+        'cns',
+        'unique:patients,cns'
+      ],
+      'birth_date' => 'required|date',
+      'gender' => 'required|string|size:1|in:M,F,O',
+      'address_id' => 'required|integer|exists:addresses,id',
+      'phone' => 'nullable|string|size:11|regex:/^[0-9]+$/',
+    ];
+  }
 }
--- backend/app/Http/Requests/UpdatePatientRequest.php
+++ backend/app/Http/Requests/UpdatePatientRequest.php
@@ -2,29 +2,37 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class UpdatePatientRequest extends FormRequest
+class UpdatePatientRequest extends StorePatientRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    $rules = parent::rules();
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+    $rules['cpf'] = [
+      'required',
+      'string',
+      'size:11',
+      'regex:/^[0-9]+$/',
+      'cpf',
+      Rule::unique('patients', 'cpf')->ignore($this->route('patient')),
+    ];
+
+    $rules['cns'] = [
+      'required',
+      'string',
+      'size:15',
+      'regex:/^[0-9]+$/',
+      'cns',
+      Rule::unique('patients', 'cns')->ignore($this->route('patient'))
+    ];
+
+    return $rules;
+  }
 }
