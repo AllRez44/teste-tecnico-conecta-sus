4b5501ed1c6eb9c9d370f6eb49c3a89dac541476
Allan Resende <allanresendegomes@outlook.com>
2026-06-20 22:58:38 -0300

    Frontend - Remove Views: Home and About & Update router and App component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,16 +7,23 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    redirect: '/dashboard',
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/DashboardView.vue'),
+  },
+  {
+    path: '/addresses',
+    name: 'addresses',
+    component: () => import('../views/AddressesView.vue'),
+  },
+  {
+    path: '/patients',
+    name: 'patients',
+    component: () => import('../views/PatientsView.vue'),
+  },
 ]
 
 const router = new VueRouter({
