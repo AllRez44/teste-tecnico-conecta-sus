d2801e3429e82623f8cd6758430da361ebd34a10
Allan Resende <allanresendegomes@outlook.com>
2026-06-20 22:58:00 -0300

    Frontend - Add Layout: AppLayout

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <nav>
+      <router-link to="/dashboard">Dashboard</router-link> |
+      <router-link to="/addresses">Addresses</router-link> |
+      <router-link to="/patients">Patients</router-link>
+    </nav>
+    <slot />
+  </div>
+</template>
+
+<style>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
