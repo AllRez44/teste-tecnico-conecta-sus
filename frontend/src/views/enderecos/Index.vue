19d77105b755aa849a3ce6e1ea5abd0cf7d53d49
Allan Resende <allanresendegomes@outlook.com>
2026-06-21 17:06:28 -0300

    Frontend - Update patients and addresses views: Update styling, implement PageLayout and PageHeader, implement reactive UI, number formatters and data fetching

--- frontend/src/views/enderecos/Index.vue
+++ frontend/src/views/enderecos/Index.vue
@@ -1,17 +1,58 @@
 <script>
 import PageHeader from "@/components/PageHeader.vue";
 import PageLayout from "@/components/layouts/PageLayout.vue";
+import TablePaginated from "@/components/TablePaginated.vue";
+import { formatApiSearchParams, formatCEP } from "@/utils";
 
 export default {
   name: "AddressesView",
+  components: {
+    PageHeader,
+    PageLayout,
+    TablePaginated
+  },
+  computed: {
+    items() {
+      return this.$store.state.addresses.addresses;
+    },
+    totalRows() {
+      return this.$store.state.pagination.addresses?.totalRows || 0;
+    }
+  },
+  data() {
+    return {
+      fields: [
+        { key: 'zip_code', label: 'CEP', thClass: 'font-weight-bold text-dark' },
+        { key: 'street', label: 'Logradouro', thClass: 'font-weight-bold text-dark' },
+        { key: 'neighborhood', label: 'Bairro', thClass: 'font-weight-bold text-dark' },
+        { key: 'city', label: 'Cidade', thClass: 'font-weight-bold text-dark' },
+        { key: 'state', label: 'UF', thClass: 'font-weight-bold text-dark' },
+        { key: 'acoes', label: 'Ações', thClass: 'font-weight-bold text-dark text-right', tdClass: 'text-right' }
+      ]
+    }
+  },
   methods: {
     handleNewAddress() {
       console.log('Novo Endereço');
     },
+    handleEdit(item) {
+      console.log('Editar', item);
+    },
+    handleDelete(item) {
+      console.log('Excluir', item);
+    },
+    async fetchAddresses() {
+      const searchParams = formatApiSearchParams(this.$route.query);
+      await this.$store.dispatch('getAddresses', { searchParams });
+    },
+    formatCEP
   },
-  components: {
-    PageHeader,
-    PageLayout,
+  watch: {
+    '$route.query.page': 'fetchAddresses',
+    '$route.query.search': 'fetchAddresses',
+  },
+  async beforeMount() {
+    await this.fetchAddresses();
   }
 }
 </script>
@@ -28,9 +69,23 @@ export default {
         </b-button>
       </template>
     </PageHeader>
-    <div /> 
+    
+    <TablePaginated
+      :items="items"
+      :fields="fields"
+      :total-rows="totalRows"
+    >
+      <template #cell(zip_code)="data">
+        {{ formatCEP(data.item.zip_code) }}
+      </template>
+      <template #cell(acoes)="data">
+        <b-button variant="link" class="p-0 mr-3 text-primary" @click="handleEdit(data.item)">
+          <b-icon icon="pencil" font-scale="1.2" />
+        </b-button>
+        <b-button variant="link" class="p-0 text-danger" @click="handleDelete(data.item)">
+          <b-icon icon="trash" font-scale="1.2" />
+        </b-button>
+      </template>
+    </TablePaginated>
   </PageLayout>
 </template>
-
-<style scoped>
-</style>
--- frontend/src/views/pacientes/Index.vue
+++ frontend/src/views/pacientes/Index.vue
@@ -1,17 +1,59 @@
 <script>
 import PageHeader from "@/components/PageHeader.vue";
 import PageLayout from "@/components/layouts/PageLayout.vue";
+import TablePaginated from "@/components/TablePaginated.vue";
+import { formatDate, formatApiSearchParams, formatCPF, formatCNS } from "@/utils";
 
 export default {
   name: "PatientsView",
+  components: {
+    PageHeader,
+    PageLayout,
+    TablePaginated
+  },
+  computed: {
+    items() {
+      return this.$store.state.patients.patients;
+    },
+    totalRows() {
+      return this.$store.state.pagination.patients?.totalRows || 0;
+    }
+  },
+  data() {
+    return {
+      fields: [
+        { key: 'name', label: 'Nome', thClass: 'font-weight-bold text-dark' },
+        { key: 'cpf', label: 'CPF', thClass: 'font-weight-bold text-dark' },
+        { key: 'cns', label: 'CNS', thClass: 'font-weight-bold text-dark' },
+        { key: 'birth_date', label: 'Data de Nascimento', thClass: 'font-weight-bold text-dark' },
+        { key: 'acoes', label: 'Ações', thClass: 'font-weight-bold text-dark text-right', tdClass: 'text-right' }
+      ]
+    }
+  },
   methods: {
     handleNewPatient() {
       console.log('Novo Paciente');
     },
+    handleEdit(item) {
+      console.log('Editar Paciente', item);
+    },
+    handleDelete(item) {
+      console.log('Excluir Paciente', item);
+    },
+    async fetchPatients() {
+      const searchParams = formatApiSearchParams(this.$route.query);
+      await this.$store.dispatch('getPatients', { searchParams });
+    },
+    formatDate,
+    formatCPF,
+    formatCNS,
   },
-  components: {
-    PageHeader,
-    PageLayout,
+  watch: {
+    '$route.query.page': 'fetchPatients',
+    '$route.query.search': 'fetchPatients',
+  },
+  async beforeMount() {
+    await this.fetchPatients();
   }
 }
 </script>
@@ -28,7 +70,29 @@ export default {
         </b-button>
       </template>
     </PageHeader>
-    <div /> 
+    <TablePaginated
+      :items="items"
+      :fields="fields"
+      :total-rows="totalRows"
+    >
+      <template #cell(cpf)="data">
+        {{ formatCPF(data.item.cpf) }}
+      </template>
+      <template #cell(cns)="data">
+        {{ formatCNS(data.item.cns) }}
+      </template>
+      <template #cell(birth_date)="data">
+        {{ formatDate(data.item.birth_date) }}
+      </template>
+      <template #cell(acoes)="data">
+        <b-button variant="link" class="p-0 mr-3 text-primary" @click="handleEdit(data.item)">
+          <b-icon icon="pencil" font-scale="1.2" />
+        </b-button>
+        <b-button variant="link" class="p-0 text-danger" @click="handleDelete(data.item)">
+          <b-icon icon="trash" font-scale="1.2" />
+        </b-button>
+      </template>
+    </TablePaginated>
   </PageLayout>
 </template>
 
