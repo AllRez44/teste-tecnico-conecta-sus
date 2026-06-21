e33c1e4cd393943f1ebc81602a537f60e824b92c
Allan Resende <allanresendegomes@outlook.com>
2026-06-21 16:47:35 -0300

    Frontend - Update utils: Add formatDate and formatApiSearchParams methods

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -3,3 +3,15 @@ export const API_URL = process.env.VUE_APP_API_URL;
 export const VIA_CEP_API_URL = "https://viacep.com.br/ws/:CEP/json/";
 
 export const numberFormatter = new Intl.NumberFormat('pt-BR');
+
+export const formatDate = (dateStr) => {
+  if (!dateStr) return '-';
+  const [year, month, day] = dateStr.split('-');
+  return `${day}/${month}/${year}`;
+};
+
+export const formatApiSearchParams = (query) => {
+  const q = { page: 1, ...query };
+  const searchParams = new URLSearchParams(q).toString();
+  return searchParams ? `?${searchParams}` : '';
+};
