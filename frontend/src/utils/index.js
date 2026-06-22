aee9483773e8481ae7e1e139d3439d30971ddf01
Allan Resende <allanresendegomes@outlook.com>
2026-06-21 23:48:32 -0300

    Frontend - Add coalesce for env var VUE_APP_API_URL in Utils

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.VUE_APP_API_URL;
+export const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:8080/api';
 
 export const VIA_CEP_API_URL = "https://viacep.com.br/ws/:CEP/json/";
 
